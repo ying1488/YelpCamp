5cf50f2d59fb2c3a6594a9d9bc2ac334cfbbb145
ying1488 <yingecteach@gmail.com>
2024-06-03 19:57:59 +1000

    campground Edit & update use method overide for PUT and PATCH methods

--- app.js
+++ app.js
@@ -55,6 +55,11 @@ app.get('/campgrounds/:id', async (req, res) => {
   res.render('campgrounds/show', { campground })
 })
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/edit', { campground });
+})
+
 
 
 app.listen(3000, () => {
