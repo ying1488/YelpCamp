94ef4a9e053b04bda1ba090d5d9399b6e0d2965c
ying1488 <yingecteach@gmail.com>
2024-06-03 21:33:35 +1000

    ejs mate boilerplate

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campgrounds');
 
@@ -23,12 +24,10 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
-
 const app = express();
-
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.get('/', (req, res) => {
