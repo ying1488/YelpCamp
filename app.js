e421e80b652b2ae4b560deaffc701f74e760f535
ying1488 <yingecteach@gmail.com>
2024-06-29 12:12:10 +1000

    validate review joi to validate postman to check

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 
-const { campgroundSchema } = require('./schemas.js')
+const { campgroundSchema, reviewSchema } = require('./schemas.js')
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override');
 const Campground = require('./models/campgrounds');
-
+const Review = require('./models/review')
 //connect to mongoose
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -45,6 +45,16 @@ const validateCampground = (req, res, next) => {
   }
 }
 
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  } else {
+    next();
+  }
+}
+
 app.get('/', (req, res) => {
   res.render('home')
 })
@@ -61,7 +71,6 @@ app.get('/campgrounds/new', (req, res) => {
 
 app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
   // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`)
@@ -89,6 +98,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   res.redirect('/campgrounds');
 }))
 
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  const review = new Review(req.body.review)
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+}))
+
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404))
 })
@@ -99,6 +117,10 @@ app.use((err, req, res, next) => {
 
   res.status(statusCode).render('error', { err })
 })
+
+
+
+
 
 app.listen(3000, () => {
   console.log('serving on port 3000')
