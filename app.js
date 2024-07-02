c5426e1329893a11a8b5648a31c796effcc88b85
ying1488 <yingecteach@gmail.com>
2024-07-02 16:36:05 +1000

    .

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use(session(sessionConfig));
 app.use(flash());
 app.use((req,res,next)=>{
   res.locals.success=req.flash('success');
+  res.locals.error = req.flash('error');
   next();
 })
 
