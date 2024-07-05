cb0efcdec7cecb8e7dff642ce2b8f2eb82de2d5d
ying1488 <yingecteach@gmail.com>
2024-07-05 17:14:19 +1000

    login routes - passport

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
 const User = require('../models/user');
+
 
 router.get('/register',(req,res)=>{
   res.render('users/register');
@@ -18,5 +20,14 @@ router.post('/register',catchAsync(async(req,res,next)=>{
     res.redirect('register');
   }
 }));
+
+router.get('/login',(req,res)=>{
+  res.render('users/login')
+})
+
+router.post('/login', passport.authenticate('local', {failureFlash:true, failureRedirect:'/login'}), (req,res)=>{
+req.flash('success','welcome back!');
+res.redirect('/campgrounds');
+})
 
 module.exports = router
