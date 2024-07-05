2e0bdc596186bbcdeacee9f2244d86688d320219
ying1488 <yingecteach@gmail.com>
2024-07-05 16:13:17 +1000

    Created user model configure passport

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
+const passport = require ('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user.js');
+
 const campgrounds = require("./routes/campgrounds.js");
 const reviews = require("./routes/reviews");
 
@@ -43,10 +47,24 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
   res.locals.success=req.flash('success');
   res.locals.error = req.flash('error');
   next();
+})
+
+app.get('/fakeUser',async(req,res)=>{
+  const user = new User({email:'test@gmail.com',username:'testtt'})
+  const newUser = await User.register(user,'chicken')
+  res.send(newUser)
 })
 
 app.use("/campgrounds", campgrounds);
