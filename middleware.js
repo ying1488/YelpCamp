d3339cdd3649f6d58bfed22aaf4b444aec3b1ee4
ying1488 <yingecteach@gmail.com>
2024-07-08 23:23:33 +1000

    ReturnTo behaviour for login in prompt

--- middleware.js
+++ middleware.js
@@ -1,7 +1,8 @@
 module.exports.isLoggedIn = (req,res,next)=>{
+  console.log("REQ.USER..", req.user);
   if(!req.isAuthenticated()){
     req.flash('error','you must be signed in');
-    res.redirect('/login');
+    return res.redirect('/login');
   }
   next();
 }
