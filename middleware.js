b5f7a56fc8aa730785f6f11e3d9d86161afc7cff
ying1488 <yingecteach@gmail.com>
2024-07-23 22:50:52 +1000

    adding reviews controller

--- middleware.js
+++ middleware.js
@@ -5,14 +5,14 @@ const Review =require('./models/review.js');
 
 module.exports.isLoggedIn = (req,res,next)=>{
   if(!req.isAuthenticated()){
-    req.session.returnTo = req.originalUrl;
-    req.flash('error','you must be signed in');
+    req.session.returnTo = req.originalUrl
+    req.flash('error','you must be signed in first');
     return res.redirect('/login');
   }
   next();
 }
 
-module.exports.validateCampground =  (req, res, next) => {
+module.exports.validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
   if (error) {
     const msg = error.details.map(el => el.message).join(',')
@@ -22,21 +22,21 @@ module.exports.validateCampground =  (req, res, next) => {
   }
 }
 
-module.exports.isAuthor = async(req,res,next)=>{
-  const{id} = req.params;
+module.exports.isAuthor = async (req,res,next) => {
+  const { id } = req.params;
   const campground = await Campground.findById(id);
   if(! campground.author.equals(req.user._id)){
-      req.flash('error','you do not have permission to do that !');
-      return res.redirect(`/campgrounds/${campground._id}`);
+      req.flash('error','You do not have permission to do that !');
+      return res.redirect(`/campgrounds/${id}`);
   }
   next();
 }
 
-module.exports.isReviewAuthor = async ( req,res,next ) => {
-  const{id,reviewId} = req.params;
+module.exports.isReviewAuthor = async (req,res,next) => {
+  const { id,reviewId } = req.params;
   const review = await Review.findById(reviewId);
-  if(!review.author.equals(req.user._id)){
-      req.flash('error','you do not have permission to do that !');
+  if (!review.author.equals(req.user._id)){
+      req.flash('error','You do not have permission to do that!');
       return res.redirect(`/campgrounds/${id}`);
   }
   next();
@@ -50,7 +50,7 @@ module.exports.validateReview = (req, res, next) => {
   } else {
     next();
   }
-}
+};
 
 module.exports.storeReturnTo = (req, res, next) => {
   if (req.session.returnTo) {
--- routes/users.js
+++ routes/users.js
@@ -4,48 +4,20 @@ const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 const { storeReturnTo } = require('../middleware');
 const User = require('../models/user');
+const users = require('../controllers/users');
 
-router.get('/register',(req,res)=>{
-  res.render('users/register');
-})
-router.post('/register',catchAsync(async(req,res,next)=>{
-  try{
-    const {email, username, password} = req.body;
-    const user = new User({email, username});
-    const registeredUser = await User.register(user, password);
-    req.login(registeredUser, err => {
-      if(err) return next(err);
-      req.flash('success','Welcome to YelpCamp');
-      res.redirect('/campgrounds');
-    })
-    
-  }catch(e){
-    req.flash('error', e.message);
-    res.redirect('register');
-  }
-}));
+router.get('/register',users.renderRegister);
 
-router.get('/login',(req,res)=>{
-  res.render('users/login')
-})
+router.post('/register',catchAsync(users.register));
+
+router.get('/login', users.renderLogin);
 
 router.post('/login', 
   storeReturnTo, 
   passport.authenticate('local', {failureFlash:true, failureRedirect:'/login'}), 
-  (req, res) => {
-    req.flash('success', 'Welcome back!');
-    const redirectUrl = res.locals.returnTo || '/campgrounds'; // update this line to use res.locals.returnTo now
-    res.redirect(redirectUrl);
-});
+  users.login
+  );
 
-router.get('/logout',(req,res,next)=>{
-  req.logout(function(err){
-    if(err){
-      return next(err);
-    }
-    req.flash('success','Goodbye!');
-  res.redirect('/campgrounds');
-  });
-});
+router.get('/logout',users.logout);
 
 module.exports = router
