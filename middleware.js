084230cce01f7821bbe20a81d7aa597d613a9dc1
ying1488 <yingecteach@gmail.com>
2024-07-23 21:16:50 +1000

    refactoring campgrounds controllers

--- middleware.js
+++ middleware.js
@@ -10,7 +10,7 @@ module.exports.isLoggedIn = (req,res,next)=>{
     return res.redirect('/login');
   }
   next();
-};
+}
 
 module.exports.validateCampground =  (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
@@ -20,7 +20,7 @@ module.exports.validateCampground =  (req, res, next) => {
   } else {
     next();
   }
-};
+}
 
 module.exports.isAuthor = async(req,res,next)=>{
   const{id} = req.params;
@@ -30,28 +30,27 @@ module.exports.isAuthor = async(req,res,next)=>{
       return res.redirect(`/campgrounds/${campground._id}`);
   }
   next();
-};
+}
 
-
-module.exports.isReviewAuthor = async(req,res,next)=>{
+module.exports.isReviewAuthor = async ( req,res,next ) => {
   const{id,reviewId} = req.params;
-  const campground = await Review.findById(reviewId);
+  const review = await Review.findById(reviewId);
   if(!review.author.equals(req.user._id)){
       req.flash('error','you do not have permission to do that !');
       return res.redirect(`/campgrounds/${id}`);
   }
   next();
-};
+}
 
 module.exports.validateReview = (req, res, next) => {
   const { error } = reviewSchema.validate(req.body);
   if (error) {
-    const msg = error.details.map(el => el.message).join(",");
+    const msg = error.details.map(el => el.message).join(',')
     throw new ExpressError(msg, 400)
   } else {
     next();
   }
-};
+}
 
 module.exports.storeReturnTo = (req, res, next) => {
   if (req.session.returnTo) {
