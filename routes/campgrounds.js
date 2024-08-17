d04f028e13927f9b0897b22aee063888e3c1e829
ying1488 <yingecteach@gmail.com>
2024-08-17 18:15:40 +1000

    multer for file upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const campgrounds = require ('../controllers/campgrounds.js');
 const { isLoggedIn, validateCampground, isAuthor} = require('../middleware.js')
+
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 const Campground = require('../models/campgrounds.js');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+.post(upload.array('image'),(req,res)=>{
+  console.log(req.body, req.files);
+  res.send("it worked!!!")
+})
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
 router.route('/:id')
