66191cc2eb5b822431d2c55ae134d2589eb8798c
ying1488 <yingecteach@gmail.com>
2024-11-03 16:31:56 +1100

    mongo injection

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const User = require('./models/user.js');
 const userRoutes = require('./routes/users.js')
 const campgroundsRoutes = require("./routes/campgrounds.js");
 const reviewsRoutes = require("./routes/reviews");
+const mongoSanitize = require('express-mongo-sanitize');
 
 //connect to mongoose
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -41,6 +42,9 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use( mongoSanitize({
+  replaceWith: '_',
+}),);
 
 const sessionConfig = {
   secret: "thisshouldbeabettersecret!",
@@ -63,6 +67,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
+console.log(req.query)
   res.locals.currentUser = req.user;
   res.locals.success=req.flash('success');
   res.locals.error = req.flash('error');
