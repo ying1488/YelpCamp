f2c6f928ace8ae243af145f99e346986be553ad6
ying1488 <yingecteach@gmail.com>
2024-11-06 23:01:58 +1100

    node js specification

--- seeds/index.js
+++ seeds/index.js
@@ -8,16 +8,18 @@ mongoose.connect(dbUrl, {
   useNewUrlParser: true,
 useUnifiedTopology: true,
 });
-// mongoose.connect("mongodb://localhost:27017/yelp-camp", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("Database connected");
 });
+
+// mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
